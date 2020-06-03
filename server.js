8ea7766a1663252d68c3e3d924ad54748f78c288
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-03 00:54:42 -0500

    add hbs, app terminada

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const hbs = require('hbs');
+require('./hbs/helpers');
 
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    //res.send('Hola Mundo');
 
-    let salida = {
-        
-        nombre: 'Sergio',
+    res.render('home', {
+        nombre: 'Sergio V.O',
         gitlab: 'https://gitlab.com/smavo',
-    }
-    res.send(salida);
+    });
 });
 
+app.get('/about', (req, res) => {
 
+    res.render('about');
+});
 
 app.listen(port, () => {
     console.log(`Escuchando peticiones en el puerto ${ port }`);
