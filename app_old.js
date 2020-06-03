47516bb3d22c02863d192035572c23c480eb5be5
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-02 23:29:08 -0500

    Iniciando Proyecto

---
+++ app_old.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        let salida = {
+            nombre: 'Sergio',
+            gitlab: 'https://gitlab.com/smavo',
+            edad: 24,
+            url: req.url
+        }
+
+        res.write(JSON.stringify(salida));
+        // res.write('Hola Mundo');
+        res.end();
+
+    })
+    .listen(8080);
+
+
+console.log('Escuchando en el puerto 8080');
